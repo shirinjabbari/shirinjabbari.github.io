ac3528e4b5b1ce93231bfa010bbb22da7f348926
DESKTOP-DBPUL65\Shirin <shirinjabbari799@gmail.com>
2021-10-24 12:08:05 +0330

    commit5

--- shirin4/script.js
+++ shirin4/script.js
@@ -1,4 +1,4 @@
-var names = ["Yaakove","John","Jack","jenifer","paul","frank","leonardo","ryan","laura","jim"];
+var names = ["Yaakove","John","Jen","jason","paul","frank","larry","paula","laura","jim"];
 for(var i=0 ; i < names.length; i++){
   if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
       console.log("Goodbye "+ names[i])
